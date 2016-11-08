21915b5cb69b83a2d4bcb54667f7227b14f44906
Andrii <at@winkas.dk>
2016-11-08 17:09:20 +0200

    Implemented iteration 1

--- ConsoleApplication1/Composition/Game.cs
+++ ConsoleApplication1/Composition/Game.cs
@@ -8,14 +8,93 @@ namespace ConsoleApplication1.Composition
 {
     class Game
     {
+        public void Start()
+        {
+            var tank = new BattleTank();
+            tank.Shooter = new BaseShooter();
+            tank.Mover = new TankMover();
+            tank.Move();
+            tank.Shoot();
+
+            var plane = new BattlePlane();
+            plane.Shooter = new BaseShooter();
+            plane.Mover = new PlaneMover();
+            plane.Move();
+            plane.Shoot();
+        }
     }
 
-    public class BattleTank
+    public interface IShooter
     {
+        void Shoot();
     }
 
-    public class BattlePlane
+    public interface IMover
     {
+        void Move();
+    }
+
+    public class BaseShooter : IShooter
+    {
+        public void Shoot()
+        {
+            Console.WriteLine("Base Shoot");
+
+        }
+    }
+
+    public class TankMover : IMover
+    {
+        public void Move()
+        {
+            Console.WriteLine("Tank Move");
+        }
+    }
+
+    public class PlaneMover : IMover
+    {
+        public void Move()
+        {
+            Console.WriteLine("Plane Move");
+        }
+    }
+
+    public interface IGameObject
+    {
+        void Move();
+        void Shoot();
+    }
+
+    public class BattleTank : IGameObject
+    {
+        public IShooter Shooter { get; set; }
+        public IMover Mover { get; set; }
+
+        public void Move()
+        {
+            this.Mover.Move();
+        }
+
+        public void Shoot()
+        {
+            this.Shooter.Shoot();
+        }
+    }
+
+    public class BattlePlane : IGameObject
+    {
+        public IShooter Shooter { get; set; }
+        public IMover Mover { get; set; }
+
+        public void Move()
+        {
+            this.Mover.Move();
+        }
+
+        public void Shoot()
+        {
+            this.Shooter.Shoot();
+        }
     }
 
 }
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApplication1.Composition;
 
 namespace ConsoleApplication1
 {
@@ -10,6 +11,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            var game = new Game();
+            game.Start();
+            Console.ReadLine();
         }
     }
 }
